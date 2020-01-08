a23dac3163b1ade379f40a0d5f4e8d2c9bd6d33a
Paolo Tacconi <p.tacconi@giunti.it>
2020-01-08 13:03:05 +0100

    Added version number

--- src/components/DelphiNavBar.js
+++ src/components/DelphiNavBar.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import appConstants from "../etc/appConstants";
+import version from "../etc/version";
 
 import {
   getUsername,
@@ -33,14 +36,16 @@ class DelphiNavBar extends React.Component {
 
   render() {
     let menuItems = null;
+    let contentEnd = null;
     if (this.state.username === null) {
       menuItems = (
         <Nav className='mr-auto'>
-          <Nav.Item href='/login' className='text-light'>
+          <Nav.Link href='/#' className='text-light'>
             Sistema semplificato di esecuzione task
-          </Nav.Item>
+          </Nav.Link>
         </Nav>
       );
+      contentEnd = "v." + version.version + " " + appConstants.install;
     } else {
       if (this.state.role === "admin") {
         menuItems = (
@@ -49,19 +54,21 @@ class DelphiNavBar extends React.Component {
             <Nav.Link href='/matchlist'>Visibilità</Nav.Link>
             <Nav.Link href='/userlist'>Utenti</Nav.Link>
             <Nav.Link href='/#' onClick={this.logout}>
-              Logout {this.state.username}
+              Logout
             </Nav.Link>
           </Nav>
         );
+        contentEnd = this.state.username;
       } else {
         menuItems = (
           <Nav className='mr-auto'>
             <Nav.Link href='/tasklist'>Elenco task</Nav.Link>
             <Nav.Link href='/#' onClick={this.logout}>
-              Logout {this.state.username}
+              Logout
             </Nav.Link>
           </Nav>
         );
+        contentEnd = this.state.username;
       }
     }
     return (
@@ -77,6 +84,9 @@ class DelphiNavBar extends React.Component {
         </Navbar.Brand>
         <Navbar.Brand href='#'>Delphi</Navbar.Brand>
         {menuItems}
+        <Navbar.Collapse className='justify-content-end'>
+          {contentEnd}
+        </Navbar.Collapse>
       </Navbar>
     );
   }
